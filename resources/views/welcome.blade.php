f00cd2f2cfcc91da695fafc2b1181281fce0c353
sohaila-1 <ton-email-souhailanachafi@.com>
2025-07-23 11:18:40 +0200

    photo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,8 @@
         </a>
     </div>
     <div class="my-4">
-        <img src="{{ asset('images/kanban.png') }}" alt="Illustration Kanban" class="img-fluid" style="max-height: 300px;">
+        <img src="{{ asset('images/kanban.png') }}" alt="Illustration Kanban"
+        class="img-fluid" style="max-height: 300px;">
     </div>
 
     <div class="card shadow-sm mt-5 mx-auto" style="max-width: 600px;">
